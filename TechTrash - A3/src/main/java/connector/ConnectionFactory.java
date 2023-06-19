31f2829914953a0d911614cf5af40d795b4be803
Vinicius Cestari <87344912+V1nnysx@users.noreply.github.com>
2023-06-18 21:24:12 -0300

    Update ConnectionFactory.java

--- TechTrash - A3/src/main/java/connector/ConnectionFactory.java
+++ TechTrash - A3/src/main/java/connector/ConnectionFactory.java
@@ -8,10 +8,10 @@ public class ConnectionFactory {
 
     private static String driver = "com.mysql.cj.jdbc.Driver";
     private static String user = "root";
-    private static String password = "soufoda02@";
+    private static String password = "123";
     private static String host = "localhost";
     private static String porta = "3306";
-    private static String bd = "test_login";
+    private static String bd = "techtrash_db";
 
     public Connection requestConnection() throws SQLException {
         Connection conn = null;
@@ -25,18 +25,3 @@ public class ConnectionFactory {
         return DriverManager.getConnection(url, user, password);
     }
 }
-/*public Connection obtemConexao() throws Exception{
-        try {
-            Class.forName(driver);
-            c = DriverManager.getConnection(
-                    "jdbc:mysql://" + host + ":" + porta + "/" + bd,
-                    usuario,
-                    senha
-            );
-            return c;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-}*/
